e216ad2f2a463117075b827574ff6f5946ad64b8
Juile Harrow <julie.ann.harrow@gmail.com>
2016-08-08 19:36:14 -0400

    relationship between users and recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808231752) do
+ActiveRecord::Schema.define(version: 20160808233430) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160808231752) do
     t.string   "pic_content_type"
     t.integer  "pic_file_size"
     t.datetime "pic_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
